29f3eedbca7bfc1077cc53c37f33064b2d35bd8e
ebf118 <91940040+ebf118@users.noreply.github.com>
2025-08-20 22:11:18 +0800

    added default theme in ThemeContext for darkmode switch

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,3 +1,6 @@
 import React, { createContext } from 'react';
 
-export const ThemeContext = createContext(null);
+export const ThemeContext = createContext({
+    isDarkMode: false,
+    toggleTheme: () => {},
+});
